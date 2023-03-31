5447fa59ef3e289546507982e3990466dc8c177e
Naveedaffu <affu63822@gmail.comm>
2023-03-31 13:02:11 +0530

    my added

---
+++ my/src/pages/Aboutus/index.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+
+export default function Aboutus(){
+    return(
+        <>
+<Typography>Name: Mohammad Afridi</Typography>
+<Typography>Father Name: Mohammad Yousuf</Typography>
+<Typography>Mother Name: Safia</Typography>
+<Typography>Email: affu63822@gmail.com</Typography>
+<Typography>Phone No.: 7993282634</Typography>
+        </>
+        
+    )
+    }
